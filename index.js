fdc3968a3827d35db1a403020ef8d8da2375f0a4
Kevin Rogers <kevinarogers1@gmail.com>
2021-12-20 20:35:43 -0500

    commit tree 31f664694c8e609e0903a79c745bc924a0aed9bf

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
-}
+  const output = tutorials.map(function (str) {
+    const words = str.split(' ').map(word => word[0].toUpperCase() + word.slice(1));
+     return words.join(' ');
+  });
+  return output;
+};
